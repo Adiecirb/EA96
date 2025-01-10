b98f711e370e28292bf6f937d38091ce4885d099
Adiecirb <briceidamh@comunidad.unam.mx>
2025-01-09 21:32:27 -0600

    fvsv

---
+++ Practica1E2.c
@@ -0,0 +1,31 @@
+//
+// Created by usuario on 09/01/2025.
+//
+#include <iso646.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void reverseString() {
+    char cadenaOriginal[50];
+    scanf("%s", cadenaOriginal);
+    printf("%s\n", cadenaOriginal);
+    int len = strlen(cadenaOriginal);
+    for(int i = len; i >= 0; i--) {
+        printf("%c", cadenaOriginal[i-1]);
+    }
+}
+
+
+
+ int main(){
+ char cadenaOriginal[50], cadenaReversa[50] ;
+
+    printf("\n Cadena reversed\n");
+    reverseString();
+
+
+
+   return 0;
+
+ }
