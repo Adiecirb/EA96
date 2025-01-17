e393084f748666bad65b8238550a621b97624ae8
Adiecirb <briceidamh@comunidad.unam.mx>
2025-01-16 19:20:49 -0600

    p6

---
+++ P6.c
@@ -0,0 +1,29 @@
+//
+// Created by usuario on 15/01/2025.
+//
+#include <stdio.h>
+
+int main(){
+ int arr[]={1,2,3,4,5};
+ int d = 5;
+
+ for (int i = 0; i < 5; i++) {
+     if (arr[i] > d){
+         printf("%d ",arr[i]);
+     }
+ }
+    //int* rotateLeft(int d, int arr_count, int* arr, int* result_count) {
+ int rotation;
+ for (int i = 0; i < arr_count; i++) {
+     arr[i];
+     rotation = arr[i - d];
+
+
+     printf("%d %d", arr[i], rotation);
+ }
+
+ return result_count;
+}
+
+
+}
